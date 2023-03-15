ddcf51d30b3c148eed5bb85955de83f52a246445
Yakubenka Ivan <palasja@gmail.com>
2023-03-15 20:58:10 +0300

    feat: add card at home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Component, ReactNode } from "react";
 import "./home.css";
+import items from "../../assets/items/items";
+import Card from "../../components/card/card";
+import { Item } from "../../types";
 
 class Home extends Component{
+    
     render(): ReactNode {
-        return <p>Home</p>
+        // const content =  items.map((item:Item): ReactNode => <Card item={item})
+    
+        return <div className="main">
+            {items.map((item:Item): ReactNode => {
+                return <Card item={item} key={item.id}/>;
+            })}
+        </div>
     }
 }
 
