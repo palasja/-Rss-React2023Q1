b9b486e38b07bcd62fe41f6be3df3e561419d575
Yakubenka Ivan <palasja@gmail.com>
2023-03-17 17:14:08 +0300

    feat: add test on card, about

---
+++ src/pages/about/about.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { getByText, render, screen } from "@testing-library/react";
+import About from ".";
+
+test("shop name on page", () => {
+  render(<About />);
+  expect(screen.getByText(/food shop/i)).toBeInTheDocument();
+});
+
+test("email link on page", () => {
+  render(<About />);
+  expect(screen.getByRole("link")).toHaveAttribute("href", "mailto: palasja@gmail.com");
+});
