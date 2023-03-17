190f089625faff127b0e01a3090f72a09bc79eb8
Yakubenka Ivan <palasja@gmail.com>
2023-03-17 15:22:16 +0300

    feat: add test config

---
+++ src/pages/404/error.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { getByText, render, screen } from "@testing-library/react";
+import Error from "./error";
+
+test("error page message", () => {
+  render(<Error />);
+  expect(screen.getByText(/Something goes wrong/i)).toBeInTheDocument();
+});
+
+test("error message has error class", () => {
+  render(<Error />);
+  expect(screen.getByText(/Something goes wrong/i)).toHaveClass('main_error_message');
+});
