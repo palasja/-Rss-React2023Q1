b12939afdfbb00f1a1442eb13799f2198b3822e7
Yakubenka Ivan <palasja@gmail.com>
2023-03-31 17:20:04 +0300

    refactor: curent route to function

--- src/components/curentRoute/currentRoute.tsx
+++ src/components/curentRoute/currentRoute.tsx
@@ -1,34 +1,26 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './currentRoute.css';
-class CurentRoute extends Component<object, { currentRoute: string }> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      currentRoute: '',
-    };
-  }
 
-  componentDidMount() {
+const CurentRoute = () => {
+  const [currentRoute, setCurrentRoute] = useState('');
+  useEffect(() => {
     const path = window.location.pathname;
     if (new RegExp(/^\/home$/, 'i').test(path) || path === '/') {
-      this.setState({ currentRoute: 'Home' });
+      setCurrentRoute('Home');
     } else if (new RegExp(/^\/about$/, 'i').test(path)) {
-      this.setState({ currentRoute: 'About' });
+      setCurrentRoute('About');
     } else if (new RegExp(/^\/newcard$/, 'i').test(path)) {
-      this.setState({ currentRoute: 'New Card' });
+      setCurrentRoute('New Card');
     } else {
-      this.setState({ currentRoute: '404' });
+      setCurrentRoute('404');
     }
-  }
+  }, [setCurrentRoute]);
 
-  render() {
-    return (
-      <div className="current-page">
-        <h3>{this.state.currentRoute}</h3>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="current-page">
+      <h3>{currentRoute}</h3>
+    </div>
+  );
+};
 
 export default CurentRoute;
